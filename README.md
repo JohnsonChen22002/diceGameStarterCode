f87ac8cc3aa5b17379976790d95a05aa2144ea65
Johnson Chen <johnsonchen12345@gmail.com>
2017-11-08 23:14:51 +0000

    DiceRoll

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # diceGameStarterCode
+
+1. This Project Create A Dice App To Use, Generating The Dice of 1-6
+2. This Project Function By The Use Of Many Codes To Create This Dice App, And To Roll The Dice, You Simply Click The Button To Generate A Random Number of A Dice.
+3. If I have more time, I would make this dice app into a game.
--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-$(document).ready(function(){
-  function rolldice(){
-  //Place your code here 
-
-
-  };
-  
+$(document).ready(function() {
+  $("button").click(function() {
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+    if (dice1 === 1) {
+      $("#first-die").html("<img src = images/Dice-1.svg.png> ")
+    }
+    if (dice1 === 2) {
+      $("#first-die").html("<img src = images/Dice-2.svg.png> ")
+    }
+    if (dice1 === 3) {
+      $("#first-die").html("<img src = images/Dice-3.svg.png> ")
+    }
+    if (dice1 === 4) {
+      $("#first-die").html("<img src = images/Dice-4.svg.png> ")
+    }
+    if (dice1 === 5) {
+      $("#first-die").html("<img src = images/Dice-5.svg.png> ")
+    }
+    if (dice1 === 6) {
+      $("#first-die").html("<img src = images/Dice-6.svg.png> ")
+    }
+    if (dice2 === 1) {
+      $("#second-die").html("<img src = images/Dice-1.svg.png> ")
+    }
+    if (dice2 === 2) {
+      $("#second-die").html("<img src = images/Dice-2.svg.png> ")
+    }
+    if (dice2 === 3) {
+      $("#second-die").html("<img src = images/Dice-3.svg.png> ")
+    }
+    if (dice2 === 4) {
+      $("#second-die").html("<img src = images/Dice-4.svg.png> ")
+    }
+    if (dice2 === 5) {
+      $("#second-die").html("<img src = images/Dice-5.svg.png> ")
+    }
+    if (dice2 === 6) {
+      $("#second-die").html("<img src = images/Dice-6.svg.png> ")
+    }
+  });
 });
